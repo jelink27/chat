cbe4ed85b572f93f2309c5d64ffd5770c79da1b7
jelink27 <61494324+jelink27@users.noreply.github.com>
2021-10-07 20:35:20 +0800

    chat

---
+++ exam.py
@@ -0,0 +1,36 @@
+import os
+
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+                for line in f:
+                    lines.append(line.strip())
+    return lines
+
+def converf(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):   
+    with open(filename, 'w',encoding='utf-8-sig') as f:
+        for line in lines:
+            f.write(line + '\n')
+ 
+def main():
+    lines = read_file('input.txt')
+    lines = converf(lines)   
+    write_file('output.txt', lines)               
+
+
+
+main()
